e291a386e14e24f79918392f330b36e9c2116a91
fish-dt <fissehadesta70@gmail.com>
2025-10-11 15:15:12 +0300

    Enhance role-based access control and session management in authentication flow
    - Updated middleware to include role checks for new routes: payment-manager, delivery, marketing, and support.
    - Refactored LoginPage to streamline user redirection based on roles, improving user experience during login.
    - Enhanced session and JWT callbacks to ensure user roles are correctly assigned and fetched from the database when necessary.
    - Added logging for better debugging of session and token handling processes.

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,10 +3,29 @@ import "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
-      id: string;       // <-- your new field
+      id: string;
+      role: string;
+      firstName: string;
+      lastName: string;
       name?: string | null;
       email?: string | null;
       image?: string | null;
     };
+  }
+
+  interface User {
+    id: string;
+    role: string;
+    firstName: string;
+    lastName: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    role: string;
+    firstName: string;
+    lastName: string;
   }
 }
